05aeb6fd9d33491b3701b22130b60323361d9ece
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-01-16 11:44:43 +0530

    Create factorial.java

---
+++ Functions and Methods/factorial.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class factorial {
+    public static int findFactorial(int x) {
+        int factorial = 1;
+        for (int i = 1; i <= x; i++) {
+            factorial *= i;
+        }
+        return factorial;
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        // System.out.println(findFactorial(a));
+        int result = findFactorial(a);
+        System.out.println(result);
+    }
+}
