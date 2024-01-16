1902d51b27364b24d83e2c29dc13cdda0afa7524
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-01-16 11:56:02 +0530

    Update factorial.java

--- Functions and Methods/factorial.java
+++ Functions and Methods/factorial.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 class factorial {
+    // create function to find Factorial
     public static int findFactorial(int x) {
         int factorial = 1;
         for (int i = 1; i <= x; i++) {
