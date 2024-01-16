4845635a66ffb240c8ef7ad972073e288f586b02
Abhinav Ashish <100207127+abhinavashish15@users.noreply.github.com>
2024-01-16 11:57:13 +0530

    Update binomialCoefficient.java

--- Functions and Methods/binomialCoefficient.java
+++ Functions and Methods/binomialCoefficient.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 public class binomialCoefficient {
+    // function to find factorial
     public static int factorial(int x) {
         int f = 1;
         for (int i = 1; i <= x; i++) {
@@ -8,11 +9,13 @@ public class binomialCoefficient {
         }
         return f;
     }
-
+    
     public static int binCoeff(int x, int y) {
+        // use factorial function for binary coefficients
         int a = factorial(x);
         int b = factorial(y);
         int c = factorial(x - y);
+        // for the formula nCr= n!/(r!*(n-r)!)
         int result = a / (b * c);
         return result;
     }
